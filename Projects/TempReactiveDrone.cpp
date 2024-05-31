180c98ff87b449ba828456efd962372263b4ada4
csaail <87662482+csaail@users.noreply.github.com>
2024-05-31 18:55:21 +0530

    Update TempReactiveDrone.cpp

--- Projects/TempReactiveDrone.cpp
+++ Projects/TempReactiveDrone.cpp
@@ -1,33 +1,62 @@
 /* Do not remove the include below */
 #include "PlutoPilot.h"
-#include "Sensor.h"
 #include "Utils.h"
+#include "Sensor.h"
 
-int16_t temp;
+int16_t initemp;
+int16_t currtemp;
 
 /* The setup function is called once at Pluto's hardware startup */
-void plutoInit()
+void plutoInit() 
 {
     /* Add your hardware initialization code here */
 }
 
 /* The function is called once before plutoLoop when you activate Developer Mode */
-void onLoopStart()
+void onLoopStart() 
 {
-    /* do your one time tasks here */
+    /* Do your one-time tasks here */
+    LED.flightStatus(DEACTIVATE); /* Disable the default LED behavior */
+    LED.set(BLUE, ON);
+    initemp = Barometer.get(TEMPERATURE) / 100; /* Get initial temperature when you turn ON the developer mode */
+    Monitor.print("initemp", initemp);
 }
 
 /* The loop function is called in an endless loop */
-void plutoLoop()
+void plutoLoop() 
 {
     /* Add your repeated code here */
-    temp = Barometer.get(TEMPERATURE) / 100; /* Get current Temperature */
-    Graph.red(temp, 1);
-    Monitor.println("Temperature", temp);
+    currtemp = Barometer.get(TEMPERATURE) / 100; /* Get current temperature */
+    
+    Graph.red(initemp, 1);
+    Graph.green(currtemp, 1);
+    Monitor.println("Currenttemp", currtemp);
+    
+    /* Check the relation between initial and current temperature and turn on LED for indicating different conditions */
+    if (currtemp > initemp) 
+    {
+        LED.set(RED, ON);
+        LED.set(BLUE, OFF);
+        Monitor.println("HOT");
+    } 
+    else if (currtemp < initemp) 
+    {
+        LED.set(GREEN, ON);
+        LED.set(BLUE, OFF);
+        Monitor.println("COOL");
+    } 
+    else 
+    {
+        Monitor.println("EQUAL");
+        LED.set(BLUE, ON);
+        LED.set(RED, OFF);
+        LED.set(GREEN, OFF);
+    }
 }
 
 /* The function is called once after plutoLoop when you deactivate Developer Mode */
-void onLoopFinish()
+void onLoopFinish() 
 {
-    /* do your cleanup tasks here */
+    /* Do your cleanup tasks here */
+    LED.flightStatus(ACTIVATE); /* Enable the default LED behavior */
 }
